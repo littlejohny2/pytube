344bc606fe42a3bce7c2e985cc7a2303018bdd3e
littlejohny2 <91983957+littlejohny2@users.noreply.github.com>
2023-08-05 14:29:08 -0400

    playlist command init

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from pytube import Playlist
 from pytube.cli import on_progress
 
 
-outputPath = 'out'
+outputPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'out')
 
 
 
@@ -25,15 +25,54 @@ def download(userInput: str):
         print('URL INVALID')
         return main()
 
-    for args in command:
+    for args in command[2:]:
         if args == '-a' or args == '-audio': # audio flag check
+            print(f'\n  downloading {youtubeURL.title}...')
             youtubeURL.streams.get_audio_only().download(outputPath)
         elif args == '-hr' or args == '-highres':
+            print(f'\n  downloading {youtubeURL.title}...')
             youtubeURL.streams.get_highest_resolution().download(outputPath)
         else:
+            print(f'\n  downloading {youtubeURL.title}...')
             youtubeURL.streams.get_by_itag(22).download(outputPath)
 
     print(f'\n{youtubeURL.title} downloaded to {os.path.abspath(outputPath)}')
+
+    main()
+
+
+def playlist(userInput: str):
+    command = userInput.split()
+
+    try:
+        playlistURL = Playlist(command[1])
+    except:
+        print('URL INVALID')
+        return main()
+    
+    for args in command[2:]:
+        if args == '-a' or args == '-audio':
+
+            for video in playlistURL.videos:
+                print(f'\n  downloading {video.title}...')
+                video.register_on_progress_callback(on_progress)
+                video.streams.get_audio_only().download(outputPath)
+
+        elif args == '-hr' or args == '-highres':
+
+            for video in playlistURL.videos:
+                print(f'\n  downloading {video.title}')
+                video.register_on_progress_callback(on_progress)
+                video.streams.get_highest_resolution().download(outputPath)
+
+        else:
+
+            for video in playlistURL.videos:
+                print(f'\n  downloading {video.title}')
+                video.register_on_progress_callback(on_progress)
+                video.streams.get_by_itag(22).download(outputPath)
+
+    print(f'\n{playlistURL.title} downloaded to {os.path.abspath(outputPath)}')
 
     main()
 
@@ -51,6 +90,7 @@ def unknown(userInput: str):
 
 
 def help():
+    print('\n')
     print('------------')
     print (' help page ')
     print('------------')
@@ -62,6 +102,11 @@ def help():
     print(' download (URL) [-audio] [-highres]: Downloads video through given URL')
     print('       -audio   : Downloads only audio')
     print('       -highres : Downloads highest resolution available')
+    print(' playlist (URL) [-audio] [-highres]: Downloads video from a playlist through given URL')
+    print('       -audio   : Downloads only audio')
+    print('       -highres : Downloads highest resolution available')
+    print('\n')
+
 
     main()
 
@@ -75,6 +120,8 @@ def main():
 
     if userInput.startswith('download'):
         download(userInput)
+    elif userInput.startswith('playlist'):
+        playlist(userInput)
     elif userInput.startswith('help'):
         help()
     elif userInput.startswith('clear') | userInput.startswith('cls'):
